7c78b7135a10c8bccc3ad853ff7a264e6c130026
Tahir D'Mello <tahirdme@gmail.com>
2024-09-30 20:57:27 -0700

    dispatcher + ofs update

--- wf/entrypoint.py
+++ wf/entrypoint.py
@@ -120,11 +120,10 @@ def initialize(run_name: str) -> str:
 
     print("Provisioning shared storage volume... ", end="")
     resp = requests.post(
-        # "http://nf-dispatcher-service.flyte.svc.cluster.local/provision-storage-ofs",
-        "http://nf-dispatcher-service.flyte.svc.cluster.local/provision-storage",
+        "http://nf-dispatcher-service.flyte.svc.cluster.local/provision-storage-ofs",
         headers=headers,
         json={
-            "storage_expiration_hours": 0,
+            "storage_expiration_hours": 1,
             "version": 2,
         },
     )
